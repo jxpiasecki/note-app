ba453f09c73d200cea4e1481291847f81b9d46ec
Jarek <jarek@booste.com>
2022-05-01 22:06:38 +0200

    Update gitignore

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+const validator = require('validator');
 const notes = require('./notes');
 console.log(notes.getNotes());
+console.log(validator.isEmail('aaaa@localhost'));
+console.log(validator.isURL('wp.pl'));
+console.log(validator.isURL('gówno.pl'));
